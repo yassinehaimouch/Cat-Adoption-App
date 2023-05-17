9e802a01e068fb7850d5ceea18689a49db15f6f3
yassinehaimouch <haymouch53@gmail.com>
2023-05-17 13:13:50 +0100

    Adding the Filter Component

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Filter.css';
+
+const Filter = () => {
+  return (
+    <div className='pets-filter-container'>
+      <div className='filter-container'>
+        <label htmlFor="favourite">Favourite</label>
+        <select name="favourite" id="favourite" className='form-select'>
+          <option value="any">Any</option>
+          <option value="favoured">Favoured</option>
+          <option value="not favoured">Not Favoured</option>
+        </select>
+      </div>
+      <div className='filter-container'>
+        <label htmlFor="gender">Gender</label>
+        <select name="gender" id="gender" className='form-select'>
+          <option value="any">Any</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+      </div>
+    </div>
+  )
+}
+
+export default Filter
